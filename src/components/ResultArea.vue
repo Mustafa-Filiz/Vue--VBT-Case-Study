d094deb7135db33b63d4245298e824642042dbaf
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-30 14:55:46 +0300

    card comp imported and search bar activated

--- src/components/ResultArea.vue
+++ src/components/ResultArea.vue
@@ -7,15 +7,20 @@
       v-model="searchText"
     />
     <div class="d-flex flex-wrap justify-content-center">
-      <!-- card -->
+      <Card v-for="image in filteredImages" :key="image.id" :image="image" />
     </div>
     <!-- pagination -->
   </div>
 </template>
 
 <script>
+import Card from "./Card.vue";
+
 export default {
   name: "ResultArea",
+  components: {
+    Card,
+  },
   data() {
     return {
       searchText: "",
@@ -25,6 +30,13 @@ export default {
     images: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    filteredImages() {
+      return this.images.filter((image) =>
+        image.header?.toLowerCase().includes(this.searchText.toLowerCase())
+      );
     },
   },
 };
